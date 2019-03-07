ebce797078f48174107b07b4a3c2af4ea8c80efc
Damian Rosinski <d.rosinski256@gmail.com>
2019-03-07 16:25:16 +0100

    added damians Branch

--- Task2c.py
+++ Task2c.py
@@ -1,6 +1,12 @@
 import string, copy
 
 
+def p(item):
+    print("-----")
+    print(item)
+    print("-----")
+
+
 def encrypt(word, key):
     alphabet = string.ascii_uppercase
     order = []
@@ -12,7 +18,9 @@ def encrypt(word, key):
             if char == key[letter_number]:
                 order[letter_number] = number
                 number += 1
+
     table = [""] * len(key)
+
     for row in range(len(table)):
         table[row] = [""] * len(key)
 
@@ -27,23 +35,44 @@ def encrypt(word, key):
     for i in range(number_of_tables):
         list_of_table.append(copy.deepcopy(table))
     number_word_letter = 0
+    index_to_skip = 0
+    flag = True
+    actuall_row_length = order.index(index_to_skip)
     for tab in range(len(list_of_table)):
         for row in range(len(list_of_table[tab])):
-            for col in range(len(list_of_table[tab][row])):
-                if list_of_table[tab][row][col] == '\u0000':
+            for col in range(len(list_of_table[tab][row]) + 1):
+                if( actuall_row_length < 0 or col == len(list_of_table[tab])):
+                    index_to_skip += 1
+                    actuall_row_length = order.index(index_to_skip)
+                    break
+                else:
+                    list_of_table[tab][row][col] = word[number_word_letter]
+                    number_word_letter += 1
+                    actuall_row_length -= 1
+                '''if list_of_table[tab][row][col] == '\u0000':
                     if number_word_letter == len(word):
                         break
                     else:
                         list_of_table[tab][row][col] = word[number_word_letter]
-                        number_word_letter += 1
+                        number_word_letter += 1'''
     encrypted = ''
-   # zczytac tylko zostało :p
+    # zczytac tylko zostało :p
+    #tablica N na N (N x N)
+
+    N = int(max(order))
+    for tab in range(len(list_of_table)):
+        for col in range(N+1):
+            for row in range(N+1):
+                print(list_of_table[tab][row][col], end="")
+
+    #p((list_of_table[0][0][0]))    #[tablica][wiersz][kolumna]
+
 
     return encrypted
 
 
 def main():
-    print(encrypt('ALA_MA_KOTA', 'ALA'))
+    print(encrypt('HEREISASECRETMESSAGEENCIPHEREDBYTRANSPOSITION', 'CONVENIENCE'))
 
 
 if __name__ == '__main__':
