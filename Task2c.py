8775530f337ff811a51e307f02360328cf348d8b
Damian Rosinski <d.rosinski256@gmail.com>
2019-03-08 18:41:41 +0100

    Working Encryption 2c

--- Task2c.py
+++ Task2c.py
@@ -1,3 +1,4 @@
+import math
 import string, copy
 
 
@@ -11,6 +12,7 @@ def encrypt(word, key):
     alphabet = string.ascii_uppercase
     order = []
     number = 0
+    encrypted = ''
     for i in range(len(key)):
         order.append("")
     for char in alphabet:
@@ -24,56 +26,75 @@ def encrypt(word, key):
     for row in range(len(table)):
         table[row] = [""] * len(key)
 
-    for row in range(len(table)):
-        for col in range(len(table[row])):
-            if col <= order[row]:
-                table[row][col] = "\u0000"
-    number_of_tables = int(len(word) / (((1 + len(key)) * len(key)) / 2))
-    if len(word) % (((1 + len(key)) * len(key)) / 2) != 0:
-        number_of_tables += 1
+    word_length = len(word)
+    N = max(order) + 1
+    number_word_in_table = ((1 + (max(order)+1))*len(key))/2 #Maximum amount of letters in table NxN -> S(n) =(S(1)+S(N)*N)/2
+
+    number_of_tables = math.ceil(word_length / number_word_in_table)
+
     list_of_table = []
     for i in range(number_of_tables):
         list_of_table.append(copy.deepcopy(table))
+
     number_word_letter = 0
-    index_to_skip = 0
-    flag = True
-    actuall_row_length = order.index(index_to_skip)
+
     for tab in range(len(list_of_table)):
+        counter = 0
         for row in range(len(list_of_table[tab])):
-            for col in range(len(list_of_table[tab][row]) + 1):
-                if( actuall_row_length < 0 or col == len(list_of_table[tab])):
-                    index_to_skip += 1
-                    actuall_row_length = order.index(index_to_skip)
-                    break
-                else:
+            index_to_skip = order.index(counter)
+            for col in range(index_to_skip + 1):
+                if  number_word_letter<len(word):
                     list_of_table[tab][row][col] = word[number_word_letter]
                     number_word_letter += 1
-                    actuall_row_length -= 1
-                '''if list_of_table[tab][row][col] == '\u0000':
-                    if number_word_letter == len(word):
-                        break
-                    else:
-                        list_of_table[tab][row][col] = word[number_word_letter]
-                        number_word_letter += 1'''
-    encrypted = ''
-    # zczytac tylko zostało :p
-    #tablica N na N (N x N)
+            counter += 1
 
-    N = int(max(order))
-    for tab in range(len(list_of_table)):
-        for col in range(N+1):
-            for row in range(N+1):
-                print(list_of_table[tab][row][col], end="")
-
-    #p((list_of_table[0][0][0]))    #[tablica][wiersz][kolumna]
-
+    for col_index in range(N):
+        order_index = order.index(col_index)
+        for tab_index in range(number_of_tables):
+            for row_index in range(N):
+                encrypted += list_of_table[tab_index][row_index][order_index]
 
     return encrypted
 
+def decrypt(word, key):
+    alphabet = string.ascii_uppercase
+    order = []
+    number = 0
+    encrypted = ''
+    for i in range(len(key)):
+        order.append("")
+    for char in alphabet:
+        for letter_number in range(len(key)):
+            if char == key[letter_number]:
+                order[letter_number] = number
+                number += 1
+
+    table = [""] * len(key)
+
+    word_length = len(word)
+    N = max(order) + 1
+    number_word_in_table = ((1 + (max(order) + 1)) * len(key)) / 2  # Maximum amount of letters in table NxN -> S(n) =(S(1)+S(N)*N)/2
+
+    number_of_tables = math.ceil(word_length / number_word_in_table)
+
+    list_of_table = []
+    for i in range(number_of_tables):
+        list_of_table.append(copy.deepcopy(table))
+
+    number_word_letter = 0
+
+
+
+
+
+    for row in range(len(table)):
+        table[row] = [""] * len(key)
+
+
+
 
 def main():
-    print(encrypt('HEREISASECRETMESSAGEENCIPHEREDBYTRANSPOSITION', 'CONVENIENCE'))
-
+     print(encrypt('ALA_MA_KOTA_DOSIE_POD_KOCEM_ANTKA', 'BA'))
 
 if __name__ == '__main__':
     main()
