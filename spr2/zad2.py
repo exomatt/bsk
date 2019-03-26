9911611cd4c19757b947dff8ad0926c5ad2ad6a8
Jakub Gosk <jakub.gosk358@gmail.com>
2019-03-26 16:28:53 +0100

    Hotfix #2 Removed redundant prints

--- spr2/zad2.py
+++ spr2/zad2.py
@@ -14,7 +14,6 @@ def encrypt(seed, poly, file, file2):
     obj.lfsr(seed, poly)
     my_array = readBinFile.read_bin_file_to_bitarray(os.getcwd() + "/spr2/" + file)
     array2 = my_array[:]
-    # array3 = my_array[:]
     counter = 0
 
     for i in range(len(my_array)):
@@ -44,13 +43,9 @@ def decrypt(seed, poly, file, file2):
             print(counter)
     readBinFile.write_bin_file(os.getcwd() + "/spr2/" + file2, array3)
 
-    # print(readBinFile.compare_two_bin_files(my_array, array2))
-
 
 def main():
     print("Main")
-    # obj = LFSR.LFSR()
-    # obj.lfsr("111010", "101011")
     encrypt("010111", "110101", "smth.png", "smth.bin")
     decrypt("010111", "110101", "smth.bin", "smth2.png")
 
@@ -65,12 +60,6 @@ def makeform(root):
     txt3.grid(column=2, row=0)
     txt4 = Entry(tab, width=width)
     txt4.grid(column=3, row=0)
-    # lbl = Entry(tab, width=width)
-    # lbl.grid(column=6, row=0)
-    # obj = LFSR.LFSR()
-
-    # def clicked():
-    #     obj.lfsr(txt.get(), txt2.get())
 
     def clicked2():
         encrypt(txt.get(), txt2.get(), txt3.get(), txt4.get())
@@ -78,8 +67,6 @@ def makeform(root):
     def clicked3():
         decrypt(txt.get(), txt2.get(), txt3.get(), txt4.get())
 
-    # btn = Button(tab, text="Prep LFSR", command=clicked)
-    # btn.grid(column=4, row=0)
     btn = Button(tab, text="Encrypt", command=clicked2)
     btn.grid(column=5, row=0)
     btn = Button(tab, text="Decrypt", command=clicked3)
