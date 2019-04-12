c5353e678305bdfcec70d463f9206c32ef059f32
Mateusz Pernal <exomat@wp.pl>
2019-04-13 00:02:06 +0200

    add f_function

--- spr3/DESAlgoritm.py
+++ spr3/DESAlgoritm.py
@@ -23,11 +23,38 @@ def expand_with_E_permut(barray):
     return array_after_expand
 
 
+def chunks(l, n):
+    # For item i in a range that is a length of l,
+    for i in range(0, len(l), n):
+        # Create an index range for l of n items:
+        yield l[i:i + n]
+
+
+def f_function(barray, key):
+    own_bitarray = bitarray(barray ^ key)
+    bit_arrays = list(chunks(own_bitarray, 6))
+    number = 0
+    temp_to_add = ''
+    for arr in bit_arrays:
+        f_l_elements = arr[::5]
+        mid_elements = arr[1:5]
+        first = int(f_l_elements.to01(), 2)
+        second = int(mid_elements.to01(), 2)
+        s_box = des_key.S_BOX
+        temp_to_add += str(bin(s_box[number][first][second]))[2:].zfill(4)
+        number += 1
+    return bitarray(temp_to_add)
+
+
 if __name__ == '__main__':
     a = bitarray("1111111110100011101000111010001110100011101000111010001110100011")
+
     barrayP = init_permutation(a)
     print(barrayP)
     left, right = spliHalf(barrayP)
     print(left)
     print(right)
-    print(expand_with_E_permut(right))
+    permut = expand_with_E_permut(right)
+    print(permut)
+    print(a[:48])
+    print(f_function(permut, a[:48]))
