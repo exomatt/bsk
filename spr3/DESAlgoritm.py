72ebb9696665d1a38cc96dc058c4a2d8d223beef
Damian Rosinski <d.rosinski256@gmail.com>
2019-04-13 16:15:50 +0200

    Encryption and Decryption for files size n*64 bits

--- spr3/DESAlgoritm.py
+++ spr3/DESAlgoritm.py
@@ -2,7 +2,7 @@ from bitarray import bitarray
 
 import spr3.DES as keygenerator
 import spr3.des_key as des_key
-
+import spr2.readBinFile as readBinFile
 
 # za parametr przyjmuje bitarray :)
 
@@ -74,25 +74,77 @@ def encrypt_block(message_in, key):
     result_encryption = inverse_permutation(massage_after_iter)
     return result_encryption
 
+# przyjmuje bit array i tu i tu czyli przed klucz słowny zamienic na bitarray
+def decrypt_block(encrypted_message_in, key):
+
+    permuted_message = init_permutation(encrypted_message_in)
+    left_message, right_massage = spliHalf(permuted_message)
+
+    keys_list = keygenerator.generate_key(key)[::-1] # Reverse generated keys. [ 0, 1, 2... 15 ] -> [ 15, 14, 13... 0 ]
+
+    for i in range(16):
+        temp_left = right_massage
+        generate_key = keys_list[i]
+        f_function_result = f_function(right_massage, generate_key)
+        right_massage = left_message ^ f_function_result
+        left_message = temp_left
+
+    massage_after_iter = right_massage + left_message  # zamiana stron bo ostatni ma byc bez zamianay a petla domyslnie ostatenie tez zamieni miejscami x:D :P
+    result_decryption = inverse_permutation(massage_after_iter)
+    return result_decryption
+
+
+def encrypt_file_full_file(input_file, out, input_key):
+    message_file = readBinFile.read_bin_file_to_bitarray(input_file)
+
+    encrypted_message = bitarray()
+    for block in range(int(len(message_file)/64)):
+        block_message = message_file[(block * 64):((block+1) * 64)]
+        block_encrypted_message = encrypt_block(block_message, input_key)
+
+        encrypted_message += block_encrypted_message
+
+    readBinFile.write_bin_file(out, encrypted_message)
+
+
+def decrypt_file_full_file(input_encrypted_file, out, input_key):
+    message_encrypted_file = readBinFile.read_bin_file_to_bitarray(input_encrypted_file)
+
+    decrypted_message = bitarray()
+    for block in range(int(len(message_encrypted_file)/64)):
+        block_message = message_encrypted_file[(block * 64):((block+1) * 64)]
+        block_decrypted_message = decrypt_block(block_message, input_key)
+
+        decrypted_message += block_decrypted_message
+
+    readBinFile.write_bin_file(out, decrypted_message)
+
 
 if __name__ == '__main__':
-    a = bitarray("1111111110100011101000111010001110100011101000111010001110100011")
-    barrayP = init_permutation(a)
-    print(barrayP)
-    left, right = spliHalf(barrayP)
-    print(left)
-    print(right)
-    permut = expand_with_E_permute(right)
-    print(permut)
-    print(a[:48])
-    print("wynik F funkcji ")
-    print(f_function(right, a[:48]))
+    # a = bitarray("1111111110100011101000111010001110100011101000111010001110100011")
+    # barrayP = init_permutation(a)
+    # print(barrayP)
+    # left, right = spliHalf(barrayP)
+    # print(left)
+    # print(right)
+    # permut = expand_with_E_permute(right)
+    # print(permut)
+    # print(a[:48])
+    # print("wynik F funkcji ")
+    # print(f_function(right, a[:48]))
     input_key = bitarray('1110011111000011111100001000011110100101110000111001011001101001')
+
+    # 'smth.png' file size is 2112 bits, so -> (2112 % 64) == 0
+    encrypt_file_full_file('smth.png', 'test1_encrypted.bin', input_key)             # encrypt file which (size % 64) == 0
+    decrypt_file_full_file('test1_encrypted.bin', 'test1_decrypted.png', input_key)  # decrypt file which (size % 64) == 0
+
     message = bitarray("1111111110100011101000111010001110100011101000111010001110100011")
-    print("Encrypted: ")
-    print("at internet fund this but it difrent than our :c :/ 0001 0011 0010 0001 0010 0100 1011 0111")
-    print("ON INTERNET RESULT:" + '110100010010010100111101000011010000111000010010001001011101110')
 
-    #Ucielo początkowe zero na internecie, ale wydaje mi sie ze nie ma to znaczenia. Działa
+    print('Message - ' + str(message))
+    print("INTERNET Encrypt:" + '110100010010010100111101000011010000111000010010001001011101110') # Tak, nie ma początkowego zera
 
-    print(encrypt_block(message, input_key))
+    encrypted = encrypt_block(message, input_key)
+    print("Encrypted: " + str(encrypted))
+
+    decrypted = decrypt_block(encrypted, input_key)
+    print("Decrypted: " + str(decrypted))
