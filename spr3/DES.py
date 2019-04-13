79c4d0e2613f49e6f944d07d767349f4b92c7dd8
Damian Rosinski <d.rosinski256@gmail.com>
2019-04-13 02:21:56 +0200

    Working Keys Generating

--- spr3/DES.py
+++ spr3/DES.py
@@ -3,8 +3,9 @@ from bitarray import bitarray
 import spr3.des_key as des_key
 
 
-def generate_key(input, cycle_num):
+def generate_key(input):
     permuted_key = OwnBitarray()
+    key_list_return = []
 
     for i in range(len(des_key.PC1)):
         permuted_key.append(input[des_key.PC1[i]-1])
@@ -12,16 +13,21 @@ def generate_key(input, cycle_num):
     L_K = permuted_key[0:28]
     R_K = permuted_key[28:56]
 
-    L_K = (L_K << des_key.SHIFT[cycle_num]) # Key shifting depends on number of cycle
-    R_K = (R_K << des_key.SHIFT[cycle_num]) # Key shifting depends on number of cycle
+    for cycle_num in range(16):
 
-    LR = OwnBitarray(L_K.to01() + R_K.to01())
+        L_K = (L_K << des_key.SHIFT[cycle_num])  # Key shifting depends on number of cycle
+        R_K = (R_K << des_key.SHIFT[cycle_num])  # Key shifting depends on number of cycle
 
-    final_key = OwnBitarray()
-    for i in range(len(des_key.PC2)):
-        final_key.append(LR[des_key.PC2[i]-1])
+        LR = OwnBitarray(L_K.to01() + R_K.to01())
 
-    return final_key
+        final_key = OwnBitarray()
+        for i in range(len(des_key.PC2)):
+            final_key.append(LR[des_key.PC2[i]-1])
+        print('-----'+ str(cycle_num) +'-------')
+        print(final_key)
+        key_list_return.append(final_key)
+
+    return key_list_return
 
 
 word = bitarray(('0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111').replace(' ',''))
@@ -36,9 +42,9 @@ R = permuted_IP[32:64]
 
 #---------------------------------------- GENERATE KEY ------------------------------------------
 
-input_key = bitarray(('1110 0111 1100 0011 1111 0000 1000 0111 1010 0101 1100 0011 1001 0110 0110 1001').replace(' ',''))
+input_key = bitarray(('00010011 00110100 01010111 01111001 10011011 10111100 11011111 11110001 ').replace(' ',''))
 
-key = generate_key(input_key,0)
-
+key_list = []
+key_list = generate_key(input_key)
 
 
--- spr3/OwnBitarray.py
+++ spr3/OwnBitarray.py
@@ -4,10 +4,12 @@ from bitarray import bitarray
 class OwnBitarray(bitarray):
 
     def __lshift__(self, count):
-        return self[count:] + type(self)('0') * count
+        end = self[:count]
+        return self[count:] + end
 
     def __rshift__(self, count):
-        return type(self)('0') * count + self[:-count]
+        start = self[count:]
+        return start + self[:-count]
 
     def __repr__(self):
         return "{}('{}')".format(type(self).__name__, self.to01())
